30652ebcf1d1c6e519aef05411b843435374020b
Leonardo Azopardo <lazopard@gmail.com>
2014-01-27 18:36:30 -0500

    Stack API almost complete

--- projects/1/Stack.java
+++ projects/1/Stack.java
@@ -1,8 +1,10 @@
-
+import java.util.Iterator;
 
 public class Stack<Item> implements Iterable<Item> {
 
   private Node first = null;
+  int size = 0;
+  boolean isEmpty = true;
 
   private class Node {
     Item item;
@@ -10,13 +12,33 @@ public class Stack<Item> implements Iterable<Item> {
     Node prev;
   }
 
-  public Stack();
-  public boolean isEmpty(); 
-  public int size();
+  public Stack() { return new Stack() };
+  public boolean isEmpty() { return size > 0 ? false : true; }
+  public int size() { return size; }
   public void push();
-  public Item pop();
-  public Item peek();
-  public Iterator<Item> iterator();
+  public Item pop() { 
+  	if (this.isEmpty()) {
+		throw new RuntimeException;
+	}
+  }
+  public Item peek() {
+	if (this.isEmpty()) {
+		throw new RuntimeException;
+	}
+  }
+  public Iterator<Item> iterator() { return new ListIterator(); }
+
+  private class ListIterator implements Iterator<Item> {
+  	private Node current = first;
+
+	public boolean hasNext() { return current != null; }
+	public void remove() { /* not supported */ }
+	public Item next() {
+		Item item = current.item;
+		current = current.next;
+		return item;
+	}
+  }
   public static void main(String[] args);
 
 }
