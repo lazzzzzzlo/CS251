a6beb63cf548245a7f5c9754c2b281f4369d37b1
Leonardo Azopardo <lazopard@gmail.com>
2014-02-22 18:20:24 -0500

    Initial commit

---
+++ projects/2/SlowConvexHull.java
@@ -0,0 +1,30 @@
+public class SlowConvexHull {
+
+  //Create an empty point set that can accommodate n points
+  public SlowConvexHull(int n) {
+
+  }
+
+
+  // Add a point P(x,y) to the point set.
+  public void add(double x, double y) {
+
+  }
+
+  /* Indicate on which side of the line passing through (x0,y0) 
+    and (x1,y1) the point (x,y) is located (-1: right, 0: on the line, 1: left) */
+  public int whichSide(double x0, double y0, double x1, double y1, double x, double y) {
+
+    return 0;
+  }
+
+  
+  /* Create a main method that reads a description of the points from standard input 
+  (using StdIn.java) and prints to standard output the list of points forming the 
+  convex hull in this format: id0 (x0, y0), id1 (x1, y1), ..., idM (xM, yM) (using StdOut.java). */
+
+  public static void main(String[] args) {
+
+  }
+
+}
