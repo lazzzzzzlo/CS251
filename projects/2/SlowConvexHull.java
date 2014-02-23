c4b3f98db275cf6ce3ce70a8f17fa6001dd96dac
Leonardo Azopardo <lazopard@gmail.com>
2014-02-22 20:19:20 -0500

    all methods but convex hull implemented

--- projects/2/SlowConvexHull.java
+++ projects/2/SlowConvexHull.java
@@ -10,8 +10,21 @@ public class SlowConvexHull {
   }
 
   //Return current number of points
-  private int size() {
+  private int getNumPoints() {
     return numPoints;
+  }
+
+  //return point set
+  private Point2D [] getPointSet() {
+
+    return pointSet;
+  
+  }
+
+  //print point set
+  private void printPointSet() {
+    for(int i = 0; i < numPoints; i++)
+      StdOut.println(this.pointSet[i].toString());
   }
 
   // Add a point P(x,y) to the point set.
@@ -20,19 +33,33 @@ public class SlowConvexHull {
   }
 
   /* Indicate on which side of the line passing through (x0,y0) 
-    and (x1,y1) the point (x,y) is located (-1: right, 0: on the line, 1: left) */
-  public int whichSide(double x0, double y0, double x1, double y1, double x, double y) {
+     and (x1,y1) the point (x,y) is located (-1: right, 0: on the line, 1: left) */
+  public static int whichSide(double x0, double y0, double x1, double y1, double x, double y) {
 
-    return 0;
+    Point2D point1 = new Point2D(x0,y0);
+    Point2D point2 = new Point2D(x1,y1);
+    Point2D point3 = new Point2D(x,y);
+
+    return -Point2D.ccw(point1, point2, point3);
+
   }
 
-  
-  /* Create a main method that reads a description of the points from standard input 
-  (using StdIn.java) and prints to standard output the list of points forming the 
-  convex hull in this format: id0 (x0, y0), id1 (x1, y1), ..., idM (xM, yM) (using StdOut.java). */
+  //read points from StdIn and add them to the point set
+  private void populatePointSet() {
+
+    double x, y;
+
+    while(!StdIn.isEmpty()) {
+
+      x = StdIn.readDouble();
+      y = StdIn.readDouble();
+      this.add(x,y);
+    }
+  }
 
   public static void main(String[] args) {
-
+    SlowConvexHull convexHull = new SlowConvexHull(StdIn.readInt());
+    convexHull.populatePointSet();
   }
 
 }
