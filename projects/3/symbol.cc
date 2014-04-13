c6d50d757c813e643a972b34f1344d792f2d723b
Leonardo Azopardo <lazopard@gmail.com>
2014-04-13 02:32:08 -0400

    symbol part 1 done

--- projects/3/symbol.cc
+++ projects/3/symbol.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <stdlib.h>
 #include <stdio.h>
 #include "key.h"
@@ -41,7 +42,58 @@ void Symbol::initializeTable(){
    }
 }
 
+void get_subset_sum(char ***pass_array, int length);
+
 Key Symbol::decrypt(Key key){
+    Key decryptedKey;
 
+    int frst_subset_size = C/2; //floor(C/2) because of integer division
+    int snd_subset_size = C - frst_subset_size; //the rest of the partial password
 
+    char **frst_pass_array = (char **) malloc(sizeof(char *)*frst_subset_size);
+    char **snd_pass_array = (char **) malloc(sizeof(char *)*snd_subset_size);
+
+    std::thread frst_thread(get_subset_sum, &frst_pass_array, frst_subset_size); //get subset sums
+    std::thread snd_thread(get_subset_sum, &snd_pass_array, snd_subset_size); //get subset sums
+
+    frst_thread.join();
+    snd_thread.join();
+
+    for(int i = 0; i < frst_subset_size; i++)
+        printf("%s\n", frst_pass_array[i]);
+
+    printf("\n\n\n");
+
+    for(int i = 0; i < snd_subset_size; i++)
+        printf("%s\n", snd_pass_array[i]);
+
+    return decryptedKey;
 }
+
+void get_subset_sum(char ***pass_array, int length) {
+
+    int pass_count = 0;
+    int *indeces = (int *) calloc(length, sizeof(int));
+
+    while(1) {
+        char *possiblePass = (char *) malloc((length+1)*sizeof(char));
+        for(int j = 0; j < length; j++)
+            possiblePass[j] = ALPHABET[indeces[j]];
+        possiblePass[length] = '\0';
+        (*pass_array)[pass_count] = possiblePass;
+
+        for(int i = length - 1;;i--) {
+            if (i < 0)
+                goto end;
+            indeces[i]++;
+            if (indeces[i] == R)
+                indeces[i] = 0;
+            else
+                break;
+        }
+        pass_count++;
+    }
+end:
+    free(indeces);
+}
+
